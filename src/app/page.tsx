893c9814aafb20b23c779509a22b749d0615ebad
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-11-26 13:14:20 +0100

    add component Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './globals.css';
 import { roboto_mono } from './fonts/font';
-import { HTag } from "./components";
+import { Button, HTag } from "./components";
 
 
 export default function Page() {
@@ -9,6 +9,8 @@ export default function Page() {
     <>
       <div>
         <HTag tag='h3'>TEXT-TEST</HTag>
+        <Button appearance='primary'>Click me </Button>
+        <Button appearance='ghost'>Click me </Button>
         <h1 className={`${roboto_mono.className} text-center text-xl font-bold text-gray-900 mb-4`}>Profession designer from 0 to PRO</h1>
         <p className="text-center text-base text-gray-900 mb-4">Students will master not only the hard skills necessary to work as a web designer,
          but also soft skills - skills that will allow them to effectively interact in a team with managers,
