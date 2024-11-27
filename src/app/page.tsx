3201ec76d09e5c1757ae121b04fb98d4f536df1d
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-11-27 17:42:45 +0100

    add props arrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     <>
       <div>
         <HTag tag='h3'>TEXT-TEST</HTag>
-        <Button appearance='primary'>Click me </Button>
+        <Button appearance='primary' arrow='right'>Click me </Button>
         <Button appearance='ghost'>Click me </Button>
         <h1 className={`${roboto_mono.className} text-center text-xl font-bold text-gray-900 mb-4`}>Profession designer from 0 to PRO</h1>
         <p className="text-center text-base text-gray-900 mb-4">Students will master not only the hard skills necessary to work as a web designer,
