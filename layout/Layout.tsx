4df575673805da25bffe0549fe84186f220008f6
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-01-12 16:23:21 +0100

    created Layout

---
+++ layout/Layout.tsx
@@ -0,0 +1,10 @@
+import { LayoutProps } from './Layout.props';
+import styles from './Layout.module.css';
+import cn from 'classnames';
+
+
+export const Layout = ({children}:LayoutProps):JSX.Element => {
+  return (
+   
+  )
+}
