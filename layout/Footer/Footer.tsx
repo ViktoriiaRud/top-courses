4b64c38630d853ad88da2a850a95aee8cbcbaf8c
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-01-16 15:18:13 +0100

    add styled in footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
 
 export const Footer = ({className, ...props}:FooterProps):JSX.Element => {
   return (
     <footer className={cn(className, styles.footer)}{...props}>
-          <div>Footer</div>
+          <div>
+               OwlTop © - {format(new Date(), "yyyy")}
+          </div>
+          <a href='#' target='_blank'>User agreement</a>
+          <a href='#' target='_blank'>Privacy policy</a>
     </footer>
   );
 };
